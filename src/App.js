6e3aa30e572aab89a7face41ae93ac0df5477ddc
David Johnson <dmjohnson844@gmail.com>
2023-03-03 03:43:23 -0500

    set state & using advice api to fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { ReactComponent as Dice }  from './icon-dice.svg';
+import { ReactComponent as Divider }  from './divider.svg';
+import React from 'react';
+
+
+class Advice extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      loadedAdvice:false,
+      id:null,
+      advice:null,
+    }
+  }
+
+  getNewAdvice() {
+    const url = `https://api.adviceslip.com/advice`
+    fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+      this.setState({
+        id:data.slip.id,
+        advice: data.slip.advice,
+        loadedAdvice:true,
+      })
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+         {
+          this.state.loadedAdvice &&
+          <div>
+            <h2>#{this.state.id}</h2>
+            <br></br>
+            <h1>"{this.state.advice}"</h1>
+          </div>
+        }
+          <div> 
+           < Divider/>
+          </div>
+        <button type='button'
+        onClick={() => this.getNewAdvice()}> 
+          <Dice/>
+        </button>
+        </div>
+    )
+  }
+
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      < Advice/>
       </header>
+    
     </div>
   );
 }
